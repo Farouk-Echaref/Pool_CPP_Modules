9c31b69d7f1b5e64bc607bb1dfced46674238236
Farouk Ech-charef <fech-cha@e1r5p12.1337.ma>
2022-07-04 03:31:02 +0100

    learn 42 c++

---
+++ notions/2_stdio_streams.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   2_stdio_streams.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 03:12:55 by fech-cha          #+#    #+#             */
+/*   Updated: 2022/07/04 03:20:55 by fech-cha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	char buff[420];
+	
+	std::cout << "42 Network" << std::endl;
+	
+	std::cout << "Input string : \n";
+	std::cin >> buff; 
+	std::cout << "String you entered : [" << buff << "]" << std::endl;
+
+	return (0);
+}
