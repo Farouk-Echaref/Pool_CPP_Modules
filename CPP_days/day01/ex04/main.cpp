937121459ca547ca6c2b63bf9bac8fba84c3fb39
Farouk Ech-charef <fech-cha@e1r4p15.1337.ma>
2022-11-13 00:20:26 +0100

    CPP MOD C01

--- CPP_days/day01/ex04/main.cpp
+++ CPP_days/day01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 18:01:55 by fech-cha          #+#    #+#             */
-/*   Updated: 2022/11/13 00:03:46 by fech-cha         ###   ########.fr       */
+/*   Updated: 2022/11/13 00:19:02 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,25 @@ int main(int argc, char **argv)
 {
     if (argc == 4)
     {
-        unsigned int i = 0;
-        int found;
+        int found = 0;
         std::string temp;
         std::string line;
         std::string src(argv[1]);
         std::string s1(argv[2]);
         std::string s2(argv[3]);
         temp = src;
-        std::ofstream Dest(temp.append(".replace"));
         std::ifstream Sourc(src);
+        std::ofstream Dest(temp.append(".replace"));
         if (Sourc.is_open() && Dest.is_open())
         {
             while (std::getline (Sourc, line))
             {
                 found = line.find(s1);
-                while(found > 0)
+                while(found != -1)
                 {
                     line.erase(found,s1.length());
                     line.insert(found, s2);
-                    found = line.find(s1);
-                    i++;
+                    found = (line).find(s1, found + s1.length());
                 }
                 Dest << line << std::endl;
             }
