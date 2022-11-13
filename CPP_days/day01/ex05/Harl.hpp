ed3c6962e777150905abbaf5417ae492c4e78a0c
Farouk Ech-charef <fech-cha@e1r4p15.1337.ma>
2022-11-13 05:04:57 +0100

    CPP small update

--- CPP_days/day01/ex05/Harl.hpp
+++ CPP_days/day01/ex05/Harl.hpp
@@ -6,13 +6,12 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 00:53:50 by fech-cha          #+#    #+#             */
-/*   Updated: 2022/11/13 04:26:45 by fech-cha         ###   ########.fr       */
+/*   Updated: 2022/11/13 04:57:23 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_HPP
 # define HARL_HPP
-# define CALL_MEMBER_FN(object,ptrToMember)  ((object).*(ptrToMember))
 
 #include <iostream>
 class Harl
