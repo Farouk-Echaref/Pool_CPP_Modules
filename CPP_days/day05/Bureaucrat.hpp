0e967f9b85249c6e0c46002c24eaba9a11e84e47
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2022-12-09 03:38:14 +0100

    CPP Starting try/throw/catch

---
+++ CPP_days/day05/Bureaucrat.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 03:25:22 by fech-cha          #+#    #+#             */
+/*   Updated: 2022/12/09 03:29:32 by fech-cha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <stdexcept>
+
+class Bureaucrat
+{
+    private:
+        std::string _name;
+        
+};
+
+#endif
