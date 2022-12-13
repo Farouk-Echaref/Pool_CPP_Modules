6684487ca9c1e42139635fc27fd46a386fbe88c2
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2022-12-13 01:48:31 +0100

    c++ 05 ex00 update

--- CPP_days/day05/ex00/Bureaucrat.hpp
+++ CPP_days/day05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 03:25:22 by fech-cha          #+#    #+#             */
-/*   Updated: 2022/12/09 03:29:32 by fech-cha         ###   ########.fr       */
+/*   Updated: 2022/12/13 01:45:27 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,44 @@
 # define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <string>
 #include <stdexcept>
 
 class Bureaucrat
 {
     private:
         std::string _name;
+        int         _grade;
+    public:
+        class GradeTooHighException:public std::exception
+        {
+            public:
+                const char* what() const throw() 
+                {
+                    return ("Grade Too High.");
+                }
+        };
+        class GradeTooLowException:public std::exception
+        {
+             public:
+                const char* what() const throw() 
+                {
+                    return ("Grade Too Low.");
+                }
+        };
+        Bureaucrat(std::string name, int grade);
+        Bureaucrat(Bureaucrat const & src);
+        virtual ~Bureaucrat();
+
+        virtual Bureaucrat & operator=(Bureaucrat const & obj);
         
+        std::string    getName() const;
+        int    getGrade() const;
+        
+        void    increment();
+        void    decrement();
 };
+
+std::ostream & operator<<(std::ostream & o, Bureaucrat const & obj);
 
 #endif
