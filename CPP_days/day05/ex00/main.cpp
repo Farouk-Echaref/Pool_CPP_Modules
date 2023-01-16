ab3b5c0c561ccc899970207cdbe3ff395243ed3c
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-01-16 16:51:16 +0100

    little update

--- CPP_days/day05/ex00/main.cpp
+++ CPP_days/day05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 01:48:44 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/01/16 15:52:07 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/01/16 16:44:40 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,22 @@
 
 int main()
 {
-    try
-    {
-        Bureaucrat obj("farouk", 150);
-        std::cout << obj;
-        obj.increment();
-        obj.decrement();
-        obj.decrement();
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+    // try
+    // {
+    //     Bureaucrat obj("farouk", 150);
+    //     std::cout << obj;
+    //     obj.increment();
+    //     obj.decrement();
+    //     obj.decrement();
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    Bureaucrat obj("farouk", 150);
+    
+    Bureaucrat test(obj);
+
+    std::cout << test.getName() << std::endl ; 
     return(0);
 }
