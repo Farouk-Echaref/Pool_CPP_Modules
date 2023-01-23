b3fa311b4c5299a0081e4c011d7e3714bde5088b
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-01-23 16:14:16 +0100

    learn templates

--- notions/18templates.cpp
+++ notions/18templates.cpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 12:45:24 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/01/23 13:23:22 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/01/23 15:40:34 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,25 +32,43 @@
 
 //create an array on the stack
 
-template<typename T,int N>
-class Array
+// template<typename T,int N>
+// class Array
+// {
+//     private:
+//         T A_Array[N];
+//     public:
+//         int GetSize() const
+//         {
+//             return (N);
+//         }
+// };
+
+// int main(void)
+// {
+//     Array<float,5> arr;
+//     Array<int,10> arr2;
+//     Array<std::string ,69> arr3;
+
+//     std::cout << arr.GetSize() << std::endl;
+
+//     return (0);
+// }
+
+
+//return max using templates
+
+//use references to avoid making copies if we are going to compare large classes
+template <typename T>
+T max(T const & a, T const & b)
 {
-    private:
-        T A_Array[N];
-    public:
-        int GetSize() const
-        {
-            return (N);
-        }
-};
+    return (a >= b ? a : b);
+}
 
 int main(void)
 {
-    Array<float,5> arr;
-    Array<int,10> arr2;
-    Array<std::string ,69> arr3;
-
-    std::cout << arr.GetSize() << std::endl;
-
-    return (0);
+    int a = 5;
+    int b = 8;
+    std::cout << max(a, b) << std::endl;
+    // std::cout << max<std::string>("abc","ABC") << std::endl;
 }
