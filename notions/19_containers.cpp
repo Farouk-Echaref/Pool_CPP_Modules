014bbd2ecf96de8a8de12281330550eed7489a73
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-01-25 13:37:55 +0100

    learning the use of algorithms

--- notions/19_containers.cpp
+++ notions/19_containers.cpp
@@ -6,12 +6,13 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 13:05:33 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/01/25 13:08:52 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/01/25 13:37:07 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <array>
+#include <algorithm>
 
 int main(void)
 {
@@ -22,9 +23,27 @@ int main(void)
     // std::cout << arr[17] << std::endl;
 
     std::array<int, 10> brr;
+
     //this won't compile in debug mode
-    brr[17] = 7;
-    std::cout << brr[17] << std::endl;
+    // brr[17] = 7;
+    // std::cout << brr.size() << std::endl;
+    // std::cout << brr.data() << std::endl;
+
+    //to fill the standard array we can use 
+
+    // for (int i = 0; i < brr.size(); i++)
+    //     brr[i] = i;
+
+    //or use an iterator 
+
+    for (std::array<int, 10>::iterator i = brr.begin(); i != brr.end(); i++)
+        *i = 5;
+    std::cout << brr[2] << std::endl;
+
+    //using algorithms
+
+    std::fill(brr.begin(), brr.end(), 69);
+    std::cout << brr[2] << std::endl;
 
     return (0);
 }
