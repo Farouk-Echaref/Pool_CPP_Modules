0d2da83aef2edd980b26f6b1301de0f1b7c67451
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-01-25 18:55:41 +0100

    cpp02 update

--- CPP_days/day02/ex01/main.cpp
+++ CPP_days/day02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 15:45:13 by fech-cha          #+#    #+#             */
-/*   Updated: 2022/11/17 20:07:34 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/01/25 18:47:40 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,18 @@ int main( void )
     Fixed a;
     Fixed const b( 10 );
     Fixed const c( 42.42f );
-        Fixed const d( b );
-a = Fixed( 1234.4321f );
-std::cout << "a is " << a << std::endl;
-std::cout << "b is " << b << std::endl;
-std::cout << "c is " << c << std::endl;
-std::cout << "d is " << d << std::endl;
-std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-return 0;
+    Fixed const d( b );
+    
+    a = Fixed( 1234.4321f );
+    
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    
+    return (0);
 }
--- CPP_days/day02/ex02/main.cpp
+++ CPP_days/day02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 15:45:13 by fech-cha          #+#    #+#             */
-/*   Updated: 2022/11/26 16:01:05 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/01/25 18:54:35 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 int main( void ) 
 {
-    // Fixed a;
-    Fixed x(5), y(5);
-    // Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-    Fixed const b( Fixed( 5.05f ) / Fixed( 0 ) );
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    // Fixed x(6);
+    // Fixed y(9);
     
-    // std::cout << a << std::endl;
-    // std::cout << ++a << std::endl;
-    // std::cout << a << std::endl;
-    // std::cout << a++ << std::endl;
-    // std::cout << a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
     
     std::cout << b << std::endl;
     
-    // std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
 
     // Fixed z = x + y;
     // Fixed t = x - y;
