39bc7f28bbeaff68ede064a573447e4b73719656
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-01-26 05:49:00 +0100

    Easyfind template

---
+++ CPP_days/day08/ex00/easyfind.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 04:13:39 by fech-cha          #+#    #+#             */
+/*   Updated: 2023/01/26 05:47:55 by fech-cha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+#include <array>
+#include <list>
+
+template<typename T>
+typename T::const_iterator easyfind(T const & tmp, int a)
+{
+    for(typename T::const_iterator it = tmp.begin(); it != tmp.end(); ++it)
+    {
+        if (*it == a)
+            return (it);
+    }
+    throw std::out_of_range("Error! the element is not found");
+}
+
+
+#endif
---
+++ CPP_days/day08/ex00/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 04:39:39 by fech-cha          #+#    #+#             */
+/*   Updated: 2023/01/26 05:48:15 by fech-cha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int main()
+{
+    std::list<int> lst;
+
+    lst.push_back(1);
+    lst.push_back(4);
+    lst.push_back(3);
+    lst.push_back(4);
+    lst.push_back(23);
+    lst.push_back(10);
+    lst.push_front(-1);
+    lst.push_back(69);
+    lst.push_back(4);
+
+    try
+    {
+        std::cout << *(::easyfind(lst, 10)) << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+    return (0);
+}
