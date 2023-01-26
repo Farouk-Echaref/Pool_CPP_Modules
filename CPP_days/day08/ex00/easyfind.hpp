f503618a457a8bc5b8dcb4c17842fb104ddea66f
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-01-26 19:57:49 +0100

    correct but not in the context of the subject, should use algos instead of iterators

--- CPP_days/day08/ex00/easyfind.hpp
+++ CPP_days/day08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 04:13:39 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/01/26 05:47:55 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/01/26 19:56:53 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,19 @@
 #include <array>
 #include <list>
 
-template<typename T>
-typename T::const_iterator easyfind(T const & tmp, int a)
-{
-    for(typename T::const_iterator it = tmp.begin(); it != tmp.end(); ++it)
-    {
-        if (*it == a)
-            return (it);
-    }
-    throw std::out_of_range("Error! the element is not found");
-}
+
+//forbidden
+
+// template<typename T>
+// typename T::const_iterator easyfind(T const & tmp, int a)
+// {
+//     for(typename T::const_iterator it = tmp.begin(); it != tmp.end(); ++it)
+//     {
+//         if (*it == a)
+//             return (it);
+//     }
+//     throw std::out_of_range("Error! the element is not found");
+// }
 
 
 #endif
