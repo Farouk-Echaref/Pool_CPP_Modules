2e35d0de8a8fbde06c3df9144b35347c2e58fbf4
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-01-30 20:31:13 +0100

    add iterators for a stack container based on the underlying container

---
+++ CPP_days/day08/ex02/MutantStack.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 00:55:55 by fech-cha          #+#    #+#             */
+/*   Updated: 2023/01/30 20:22:20 by fech-cha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANT_STACK_HPP
+# define MUTANT_STACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <deque>
+#include <algorithm>
+#include <iterator>
+#include <list>
+
+template<typename T, typename Container = std::deque<T> >
+class MutantStack: public std::stack<T, Container> 
+{
+    public:
+        typedef typename Container::iterator iterator;
+        typedef typename Container::const_iterator const_iterator;
+
+
+        iterator begin() { return this->c.begin(); }
+        iterator end() { return this->c.end(); }
+
+        const_iterator begin() const { return this->c.begin(); }
+        const_iterator end() const { return this->c.end(); }
+};
+
+#endif
