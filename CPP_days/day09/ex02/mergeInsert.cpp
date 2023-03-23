b6b4f607cc753922e4fa9ea01dd3ac05ed89b07c
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-03-23 08:35:02 +0000

    Update the sorting algorithm

--- CPP_days/day09/ex02/mergeInsert.cpp
+++ CPP_days/day09/ex02/mergeInsert.cpp
@@ -6,50 +6,70 @@
 /*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 06:35:42 by fech-cha          #+#    #+#             */
-/*   Updated: 2023/03/23 07:10:36 by fech-cha         ###   ########.fr       */
+/*   Updated: 2023/03/23 08:24:12 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-void merge_insert(std::vector<int>& arr, int left, int right) {
+void    ft_swap(int *a, int *b)
+{
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
+
+void    insertSort(std::vector<int> &vec, int left, int right)
+{
+    int i,j;
+    for (i = left; i < right; i++)
+    {
+        j = i + 1;
+        while (j > 0)
+        {
+            if (vec[j - 1] > vec[j])
+                ft_swap(&vec[j - 1], &vec[j]);
+            j--;
+        }
+    }
+}
+
+void    merge(std::vector<int> &vec, int left, int mid, int right)
+{
+    std::vector<int> tmp(right - left + 1);
+        int i = left, j = mid + 1, k = 0;
+        while (i <= mid && j <= right) {
+            if (vec[i] <= vec[j]) {
+                tmp[k++] = vec[i++];
+            } else {
+                tmp[k++] = vec[j++];
+            }
+        }
+        while (i <= mid) {
+            tmp[k++] = vec[i++];
+        }
+        while (j <= right) {
+            tmp[k++] = vec[j++];
+        }
+        std::copy(tmp.begin(), tmp.end(), vec.begin() + left);
+}
+
+void    merge_insert(std::vector<int>& vec, int left, int right) {
     if (left >= right) {
         return;
     }
 
-    const int insert_sort_threshold = std::max(10, static_cast<int>(std::sqrt(arr.size())));;
+    const int insert_sort_threshold = std::max(10, static_cast<int>(std::sqrt(vec.size())));;
     if (right - left + 1 <= insert_sort_threshold) {
-        // sort subarray using insertion sort
-        for (int i = left + 1; i <= right; ++i) {
-            int key = arr[i];
-            int j = i - 1;
-            while (j >= left && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                --j;
-            }
-            arr[j + 1] = key;
-        }
-    } else {
+        // sort subvecay using insertion sort
+        insertSort(vec, left, right);
+    }
+    else {
         int mid = (left + right) / 2;
-        merge_insert(arr, left, mid);
-        merge_insert(arr, mid + 1, right);
+        merge_insert(vec, left, mid);
+        merge_insert(vec, mid + 1, right);
 
-        // merge sorted subarrays
-        std::vector<int> tmp(right - left + 1);
-        int i = left, j = mid + 1, k = 0;
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                tmp[k++] = arr[i++];
-            } else {
-                tmp[k++] = arr[j++];
-            }
-        }
-        while (i <= mid) {
-            tmp[k++] = arr[i++];
-        }
-        while (j <= right) {
-            tmp[k++] = arr[j++];
-        }
-        std::copy(tmp.begin(), tmp.end(), arr.begin() + left);
+        // merge sorted subvecays
+        merge(vec, left, mid, right);
     }
 }
